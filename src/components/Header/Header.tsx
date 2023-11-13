8b4ddbd35a729687ffa59ec73d62e68a19995656
seifeddinesaad <saadseif1925@outlook.com>
2023-11-13 20:44:40 +0000

    adding some improvements in the header and adding react routes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,29 +2,58 @@
 import React from 'react';
 import './Header.css'; // Import the CSS file
 import useWindow from '../../utils/useWindow';
+import { NavLink } from 'react-router-dom';
 
 const Header: React.FC = () => {
-  const {width} = useWindow();
+  const { width } = useWindow();
+  const links = [
+    {
+      name:"Home",
+       tag:"home"
+    },
+    {
+      name:"About",
+       tag:"about"
+ },
+    {
+      name:"Skills",
+       tag:"skills"
+    },
+    {
+      name:"Projects",
+       tag:"projects"
+    },
+    {
+      name:"Contact",
+       tag:"contact"
+    },
+  ]
   return (
     <header className="header">
       <div className="logo">Your Logo</div>
 
       {/* Menu icon for medium and small devices */}
-      <div className="menu-icon">&#9776;</div>
-      <nav className="nav-links">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Skills</a>
-        <a href="#">Projects</a>
-        <a href="#">Contact</a>
-      </nav>
 
-      <div className="icons-wrapper">
-        {/* You can replace these icons with your preferred icon components or images */}
-        <span>Icon 1</span>
-        <span>Icon 2</span>
-        <span>Icon 3</span>
-      </div>
+      {width > 765 ?
+        <>
+          <nav className="nav-links">
+            {links.map((link, index)=>{
+              return  <NavLink to={link?.tag}>{link?.name}</NavLink>
+            })}
+          </nav>
+          <div className="icons-wrapper">
+            {/* You can replace these icons with your preferred icon components or images */}
+            <span>Icon 1</span>
+            <span>Icon 2</span>
+            <span>Icon 3</span>
+          </div>
+        </>
+        :
+        <div className="menu-icon">&#9776;</div>
+      }
+
+
+
     </header>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const root = document.getElementById('root');
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  root
 );
 
 // If you want to start measuring performance in your app, pass a function
