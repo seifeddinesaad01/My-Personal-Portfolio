c16211873b7396c54a138fe8a47875eedb990ed2
seifeddinesaad <saadseif1925@outlook.com>
2023-11-15 20:31:26 +0000

    hero section last touches

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React from 'react';
+import heroImg from '../../assets/hero1.png'
 import './Hero.css'
 const Hero = () => {
-  return (
-    <div className='hero__section'>
-    <div className="hero__left">
-        <h1>SEIF SAAD</h1>
-        <h3>Web Developer</h3>
-        <button>Projects</button>
-        <button>Contact</button>
-    </div>
-    <div className="hero__right">
-<img src="sdsf" alt="cd" />
-    </div>
-    </div>
+    return (
+        <div className='hero__section'>
+            <div className="hero__left">
+                <div className="hero__info">
+                    <h1 className='hero__title'>Seif Eddine Saad</h1>
+                    <h3 className='hero__description'>Web Developer</h3>
+                    <div className="hero__buttons">
+                    <button className='hero__btn'>Projects</button>
+                    <button className='hero__btn'>Contact</button>
+                    </div>
+                    <div className="infos__box">
+                        <div className="info__box">
+                            <p>20+</p>
+                            <p>experience</p>
+                        </div>
+                    </div>
+                </div>
 
-  )
+
+            </div>
+            <div className="hero__right">
+                <img src={heroImg} alt="cd" className='hero__img' />
+            </div>
+        </div>
+
+    )
 }
 
 export default Hero
