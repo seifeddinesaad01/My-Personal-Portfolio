dd70b99811b713ed7206b5de39b98d32011459b1
seifeddinesaad <saadseif1925@outlook.com>
2023-11-16 19:13:02 +0000

    sectionOne compleated

--- src/components/SectionOne/SectionOne.tsx
+++ src/components/SectionOne/SectionOne.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import "./SectionOne.css"
 import sectionOneImg from "../../assets/about.jpg";
+import { MdDesignServices } from "react-icons/md";
+import { FaCode } from "react-icons/fa";
+import { SiTestinglibrary } from "react-icons/si";
+
 const SectionOne = () => {
   return (
     <div className="sectionOne">
       <div className="sectionOne__left">
-        <img src={sectionOneImg} alt="sectionImg" className='sectionOne__img'/>
+        <img src={sectionOneImg} alt="sectionImg" className='sectionOne__img' />
       </div>
       <div className="sectionOne__right">
         <h1>Core Feature</h1>
-        <h4>Let's work together to bring your <br /> ideas to life and create amazing <br /> software that makes a difference!</h4>
+        <h4>Let's work together to bring your ideas to life and create amazing software that makes a difference!</h4>
+        <div className="sectionOne__categories">
+          <div className="sectionOne__category">
+            <FaCode fontSize="3rem" color='#f98484' />
+            <p style={{fontSize:"1.2rem", fontWeight:"600"}}>Coding</p>
+          </div>
+          <div className="sectionOne__category">
+            <MdDesignServices fontSize="3rem" color='#f98484' />
+            <p style={{fontSize:"1.2rem", fontWeight:"600"}}>Design</p>
+          </div>
+          <div className="sectionOne__category">
+            <SiTestinglibrary fontSize="3rem" color='#f98484' />
+            <p style={{fontSize:"1.2rem", fontWeight:"600"}}>Testing</p>
+          </div>
+        </div>
+        <p className='sectionOne__text'>Hi, I'm a software engineering student and developer with <br /> a passion for solving complex problems through code.
+        </p>
+        <button className='sectionOne__btn'>Contact Me</button>
+
       </div>
     </div>
   )
