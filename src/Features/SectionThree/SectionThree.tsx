3780a0ba24a79d750fd4f8eefb26c22fc9b2da86
seifeddinesaad <saadseif1925@outlook.com>
2023-11-22 08:27:54 +0000

    implementing the layout of Secton Three

--- src/Features/SectionThree/SectionThree.tsx
+++ src/Features/SectionThree/SectionThree.tsx
@@ -2,15 +2,29 @@ import React from 'react';
 import "./SectionThree.css";
 import ProjectCard from '../../components/ProjectCard/ProjectCard';
 const projects = [
-    {}
-]
+    {
+    },
+    {
+    },
+    {
+    },
+]   
 const SectionThree = () => {
     return (
         <div className="sectionThree">
             <h1>Recent Projects</h1>
             <div className="sectionThree__bottom">
                 {projects?.map((project) => {
-                    return <ProjectCard />
+                    return <> 
+                    <ProjectCard
+                        title="Project Name"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        imageSrc="https://placekitten.com/800/400" // Replace with your actual image source
+                        githubLink="https://github.com/yourusername/yourproject"
+                        liveSiteLink="https://yoursite.com"
+                    />
+                    </>
+
                 })}
             </div>
         </div>)
--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
 import "./ProjectCard.css";
-const ProjectCard = () => {
-  return (
-    <div>ProjectCard</div>
-  )
+interface ProjectCardProps {
+  title: string;
+  description: string;
+  imageSrc: string;
+  githubLink: string;
+  liveSiteLink: string;
 }
 
-export default ProjectCard
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  title,
+  description,
+  imageSrc,
+  githubLink,
+  liveSiteLink,
+}) => {
+  return (
+    <div className="project-card">
+      <img className="project-image" src={imageSrc} alt={title} />
+      <div className="project-details">
+        <h3 className="project-title">{title}</h3>
+        <p className="project-description">{description}</p>
+        <div className="project-links">
+          <a href={githubLink} target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-github"></i> GitHub
+          </a>
+          <a href={liveSiteLink} target="_blank" rel="noopener noreferrer">
+            Live Site <i className="fas fa-external-link-alt"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
