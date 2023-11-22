31969498f8d229f6fbd6753969b5da79fb0c81a6
seifeddinesaad <saadseif1925@outlook.com>
2023-11-22 08:09:35 +0000

    removing padding from the tabs component in small devices

--- src/Features/Home/Home.tsx
+++ src/Features/Home/Home.tsx
@@ -3,14 +3,14 @@ import Hero from '../Hero/Hero'
 import SectionTwo from '../SectionTwo/SectionTwo'
 import SectionOne from '../SectionOne/SectionOne'
 import './Home.css'
-// import SectionThree from '../SectionThree/SectionThree'
+import SectionThree from '../SectionThree/SectionThree';
 const Home = () => {
   return (
     <div className='home__container'>
       <Hero />
       <SectionOne />
       <SectionTwo />
-      {/* <SectionThree /> */}
+      <SectionThree />
     </div>
   )
 }
