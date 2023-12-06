8e5d3bc8a16aed8826550d7f4a6c62b43ea83adb
seifeddinesaad <saadseif1925@outlook.com>
2023-12-06 09:53:01 +0000

    creating our checkout page

--- src/Features/Home/Home.tsx
+++ src/Features/Home/Home.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Hero from '../Hero/Hero'
 import SectionTwo from '../SectionTwo/SectionTwo'
 import SectionOne from '../SectionOne/SectionOne'
 import './Home.css'
 import SectionThree from '../SectionThree/SectionThree';
+import Spinner from "../../components/Spinner"
 const Home = () => {
   return (
-    <div className='home__container'>
-      <Hero />
-      <SectionOne />
-      <SectionTwo />
-      <SectionThree />
-    </div>
+    <>
+      <div className='home__container'>
+        <Hero />
+        <SectionOne />
+        <SectionTwo />
+        <SectionThree />
+      </div>
+    </>
+
   )
 }
 
